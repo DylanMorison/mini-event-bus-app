6af09d2eb0af44fe5d4732913f8bc012d4b2bbf6
DylanMorison <dylan@g.morison.org>
2022-12-26 15:26:48 -0800

    fetch past events on startup

--- query/index.js
+++ query/index.js
@@ -1,19 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require('axios')
 
 const posts = {}
 
-const app = express()
-
-app.use(bodyParser.json())
-app.use(cors())
-
-app.get('/posts', (req, res) => res.send(posts))
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body
-
+const handleEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data
 
@@ -32,10 +24,34 @@ app.post('/events', (req, res) => {
     comment.status = status
     comment.content = content
   }
-  console.log(posts)
+}
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/posts', (req, res) => res.send(posts))
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body
+
+  handleEvent(type, data)
+
   res.send({})
 })
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Query service listening on 4002')
+
+  try {
+    const res = await axios.get('http://localhost:4005/events')
+
+    for (const event of res.data) {
+      console.log('Processing event:', event.type)
+      handleEvent(event.type, event.data)
+    }
+  } catch (err) {
+    console.error(err)
+  }
 })
