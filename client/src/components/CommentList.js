82c0e35e72660010d78e99f7688fb1aef2a410ac
DylanMorison <dylan@g.morison.org>
2022-12-26 14:42:19 -0800

    asd

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
 const CommentList = ({ comments }) => {
-  const renderedComments = comments.map((comment) => <li key={comment.id}>{comment.content}</li>)
+  const renderedComments = comments.map((comment) => {
+    let content
+    if (comment.status === 'approved') {
+      content = comment.content
+    } else if (comment.status === 'pending') {
+      content = 'awaiting moderation'
+    } else if (comment.status === 'rejected') {
+      content = 'REJECTED '
+    }
+    return <li key={comment.id}>{content}</li>
+  })
 
   return <ul>{renderedComments}</ul>
 }
